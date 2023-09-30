65e590354b7afc22f36007da2a204cabaa2b75b4
AndKlet <andreaskletvang@outlook.com>
2023-09-30 05:36:56 +0200

    Change data types

--- Code/src/mandelbrot.c
+++ Code/src/mandelbrot.c
@@ -1,6 +1,5 @@
 #include <complex.h>
 
-
 float rmax = 1.5f;
 float rmin = -1.5f;
 // make sure the aspect ratio is 16:9
@@ -12,16 +11,24 @@ int max_iterations = 200;
 int mandelbrot(int pixilIndex, int width, int height) {
     int x = pixilIndex % width;
     int y = pixilIndex / width;
-    float x0 = (float)x / (float)width * (rmax - rmin) + rmin;
-    float y0 = (float)y / (float)height * (imax - imin) + imin;
-    float complex z0 = x0 + y0 * I;
-    float complex z = z0;
+    double x0 = (double)x / (double)width * (rmax - rmin) + rmin;
+    double y0 = (double)y / (double)height * (imax - imin) + imin;
+    double complex z0 = x0 + y0 * I;
+    double complex z = z0;
     int iterations = 0;
     while(cabsf(z) < 2.0f && iterations < max_iterations) {
         z = z*z + z0;
         iterations++;
     }
     return iterations;
+}
+
+// Set new mandelbrot range
+void set_mandelbrot_range(float new_rmin, float new_rmax, float new_imin, float new_imax) {
+    rmin = new_rmin;
+    rmax = new_rmax;
+    imin = new_imin;
+    imax = new_imax;
 }
 
 // map the number of iterations to a color
