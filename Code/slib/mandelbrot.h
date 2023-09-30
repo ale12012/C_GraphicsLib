9e30791f229f36c509e1ca26cdcffc76df6b65fe
AndKlet <andreaskletvang@outlook.com>
2023-09-30 05:36:37 +0200

    Add extern variables

--- Code/slib/mandelbrot.h
+++ Code/slib/mandelbrot.h
@@ -1,7 +1,13 @@
 #ifndef MANDELBROT_H
 #define MANDELBROT_H
 
+extern float rmax;
+extern float rmin;
+extern float imin;
+extern float imax;
+
 int mandelbrot(int pixilIndex, int width, int height);
 int color(int iterations);
+void set_mandelbrot_range(float new_rmin, float new_rmax, float newimin, float new_imax);
 
 #endif
