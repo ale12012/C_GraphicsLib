1a31862d51776fac0e768a94b47362c7251ca58a
AndKlet <andreaskletvang@outlook.com>
2023-10-01 01:26:35 +0200

    Update headerfile for new functions

--- Code/slib/mandelbrot.cuh
+++ Code/slib/mandelbrot.cuh
@@ -4,7 +4,11 @@ extern "C" {
 #include "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\include\cuda_runtime.h"
 #include "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\include\device_launch_parameters.h"
 __global__ void mandelbrotKernel(int* output, int width, int height, float rmin, float rmax, float imin, float imax, int max_iterations);  
-int color(int iterations);
+int color(int choice, int iterations);
+int defColor(int iterations);
+int fullSpectrumColor(int iterations);
+int graidiantColor(int iterations);
+int hueToRGB(float hue);
 void set_mandelbrot_range(float new_rmin, float new_rmax, float newimin, float new_imax);
 void computeMandelbrotGPU(int* h_output, int width, int height);
 int initializeCUDA();
